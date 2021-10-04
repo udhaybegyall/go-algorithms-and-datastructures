7a1c6574948d31d08c38b1832a2fb80406cbeee1
udhay <38879140+udhaybegyall@users.noreply.github.com>
2021-10-04 19:43:13 +0530

    Update stack.go

--- data-structures/stack/stack.go
+++ data-structures/stack/stack.go
@@ -6,24 +6,24 @@ type Stack struct {
 }
 
 func (s *Stack) isEmpty() bool {
-
 	return len(s.values) == 0
 }
 
 func (s *Stack) push(value int) {
-
 	s.values = append(s.values[:s.valueCount], value)
 	s.valueCount += 1
 }
 
 func (s *Stack) pop() int {
-
+	
+	// returns 0 if counter is 0, that means there were no push operations to increment the counter.
 	if s.valueCount == 0 {
 		return 0
 	}
-
+	
+	// stores the last pushed element in the value variable
 	var value = s.values[len(s.values)-1]
-
+	
 	if len(s.values) > 1 {
 		s.values = s.values[:len(s.values)-1]
 	} else {
